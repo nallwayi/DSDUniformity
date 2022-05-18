619ffbeb3f217a2629d01331a87a293b0ff8d785
Nithin Allwayin <54512005+nallwayi@users.noreply.github.com>
2022-05-18 11:10:54 -0400

    Create getPDFpropertiesTimeSeries.m
    Mean and std of hologram dist

---
+++ getPDFpropertiesTimeSeries.m
@@ -0,0 +1,23 @@
+% Function to get the mean and standard deviation of the hologram PDF's as
+% a time series
+% May 5,2022
+
+
+function[holoMean,holoStd] = getPDFpropertiesTimeSeries(pStats)
+
+
+holoTime = pStats.holoinfo(:,3);
+holonum  = pStats.holoinfo(:,1);
+holoMean = nan(numel(holoTime),1); 
+holoStd = nan(numel(holoTime),1); 
+
+for cnt = 1: numel(holoTime)
+    ind = pStats.metrics.holonum == holonum(cnt) & ...
+        pStats.metrics.majsiz > 10e-6;
+        prtcleDiamArray = pStats.metrics.majsiz(ind);
+        prtcleDiamArray(prtcleDiamArray < 10e-6) = [];
+        holoMean(cnt) = mean(prtcleDiamArray);
+        holoStd(cnt) = std(prtcleDiamArray);
+    
+end
+end
